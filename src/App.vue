3e1261e013b3ae62f5e6e09bad41d19785efa75f
Morgan Segura <morgansegura@gmail.com>
2017-11-16 10:50:13 -0800

    update image assets and make grid fully responsive

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,43 @@
 <template>
-  <div id="app">
-    <!-- the router outlet, where all matched components would ber viewed -->
-    <router-link v-bind:to="'/'">Home</router-link>
-    <router-link v-bind:to="'/work'">Work</router-link>
-    <router-link v-bind:to="'/about'">About</router-link>
-    <router-link v-bind:to="'/blog'">Blog</router-link>
-    <router-link v-bind:to="'/services'">Services</router-link>
-    <router-link v-bind:to="'/contact'">Contact</router-link>
-
-    <router-view></router-view>
+  <div id="app" class="app cf">
+    <div class="container">
+      <header class="menu">
+        <div class="logo-block">
+          <h2>Alpha</h2>
+          <p>Where it all begins</p>
+        </div>
+        <nav>
+          <router-link v-bind:to="'/'">Home</router-link>
+          <router-link v-bind:to="'/work'">Work</router-link>
+          <router-link v-bind:to="'/about'">About</router-link>
+          <router-link v-bind:to="'/blog'">Blog</router-link>
+          <router-link v-bind:to="'/services'">Services</router-link>
+          <router-link v-bind:to="'/contact'">Contact</router-link>
+        </nav>
+        <div class="filter">
+          <h3>Filter</h3>
+          <ul>
+            <li><a href="">All Works</a></li>
+            <li><a href="">web design</a></li>
+            <li><a href="">illustration</a></li>
+            <li><a href="">photography</a></li>
+            <li><a href="">wallpapers</a></li>
+            <li><a href="">brochures</a></li>
+          </ul>
+        </div>
+        <div class="social-media">
+          <a href=""><i class="fa fa-tumbler"></i></a>
+          <a href=""><i class="fa fa-google-plus"></i></a>
+          <a href=""><i class="fa fa-twitter"></i></a>
+          <a href=""><i class="fa fa-pinterest"></i></a>
+          <a href=""><i class="fa fa-dribbble"></i></a>
+          <a href=""><i class="fa fa-tumbler"></i></a>
+          <a href=""><i class="fa fa-facebook"></i></a>
+        </div>
+        <p class="copyright">&copy 2017 Alpha. All Rights Reserved.</p>
+      </header>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -18,13 +47,4 @@ export default {
 }
 </script>
 <!-- styling for the component -->
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,29 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="main">
+    <div class="grid cf">
+      <div class="grid-cell"><img src="../assets/images/grid1.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid2.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid3.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid4.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid5.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid6.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid7.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid8.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid9.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid10.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid11.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid12.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid13.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid14.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid15.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid16.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid17.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid18.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid19.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid20.jpg" /></div>
+      <div class="grid-cell"><img src="../assets/images/grid21.jpg" /></div>
+      
+  </div>
   </div>
 </template>
 
@@ -32,19 +39,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
